1c0038c0b5d167759f13ce289bcc2aa9117d527c
randallreedjr <randallreedjr@gmail.com>
2014-06-11 16:59:56 -0400

    Make profile_url_grabber work

--- webscraper.rb
+++ webscraper.rb
@@ -20,40 +20,37 @@ def profile_grabber
   names = name_grabber(doc)
   tags = tag_grabber(doc)
   excerpts = excerpt_grabber(doc)
+  profile_urls = profile_url_grabber(doc)
   names.each_with_index do |student_name, index|
     students << {:name => student_name} 
     students[index][:excerpt] = excerpts[index]
     students[index][:tag_line] = tags[index]
   end
-  return students
+  #return students
+  return profile_urls
 end
 
-
-
 def name_grabber(doc)
-  profiles = [] 
-  # Student names
   profiles = doc.search("h3 a").collect{|e| e.text.strip }
   return profiles
 end
 
 def excerpt_grabber(doc)
-  excerpts = []
   excerpts = doc.search(".excerpt p" , ".Learning p").collect{|e| e.text.strip } 
   return excerpts
 end
 
-
-
 def tag_grabber(doc)
-  tags = []
   tags = doc.search(".home-blog-post-meta").collect{|e| e.text.strip }
   return tags
 end
 
+def profile_url_grabber(doc)  
+  profile_urls = doc.search(".blog-thumb a").collect{|e| e["href"].strip }
+  return profile_urls
+end
 
-
-
+ #to-do
 def image_url_grabber
   profiles = []
   doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
@@ -65,18 +62,9 @@ def image_url_grabber
   return image_url
 end
 
-def profile_url_grabber
-  profiles = []
-  doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
-  profiles = doc.search(".blog-thumb a").collect{|e| e.text.strip }
-  profile_url = []
-  profiles.each do |profile_url_name|
-    profile_url << {:profile_url => profile_url_name} 
-  end
-  return profile_url
-end
 
-# puts profile_grabber
+
+#puts profile_grabber
 
 
 
