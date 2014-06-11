93b34faa1838b4835aafcdf21987c3e8c9bde8de
ablwr <ashley.blewer@gmail.com>
2014-06-11 13:51:44 -0400

    namegrabber created

---
+++ webscraper.rb
@@ -0,0 +1,31 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+# html = open('http://ruby005.students.flatironschool.com/').read
+
+# URLHacking
+
+def name_grabber
+profiles = []
+  doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
+  profiles << doc.search("h3 a").collect{|e| e.text.strip }
+  profiles
+end
+
+puts profiles
+# Other way cool too
+# while i < 10
+#   next_page = docsearch("a.button.next").attribute("href")
+#   doc = Nokogiri::HTML(open("http://newyork.craigslist.org/roo/#{next_page}"))
+#   prices << doc.search("span.price").collect{|e| e.text.gsub("$","").to_i}
+#   i+=1
+# end
+
+
+
+
+
+# [1,2].inject(0) do |total, element|
+#   total + element
+# end
