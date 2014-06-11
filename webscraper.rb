ac9398c89d0e1ac1ecb2e7ea0d6cc567257f3ce5
dsully360 <dsully15@gmail.com>
2014-06-11 16:08:10 -0400

    suck it

--- webscraper.rb
+++ webscraper.rb
@@ -15,19 +15,42 @@ require 'pry'
 # one hash per student, and put them in an array of hashes
 
 def profile_grabber
-end
-
-def name_grabber
-  profiles = []
-  doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
-  # Student names
-  profiles << doc.search("h3 a").collect{|e| e.text.strip }
   students = []
-  profiles.each do |student_name|
+  doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
+  names = name_grabber(doc)
+  excerpts = excerpt_grabber(doc)
+  names.each_with_index do |student_name, index|
     students << {:name => student_name} 
+    students[index][:excerpt] = excerpts[index]
   end
   return students
 end
+
+
+
+def name_grabber(doc)
+  profiles = [] 
+  # Student names
+  profiles = doc.search("h3 a").collect{|e| e.text.strip }
+  return profiles
+end
+
+def excerpt_grabber(doc)
+  excerpts = []
+  excerpts = doc.search(".excerpt p" , ".Learning p").collect{|e| e.text.strip } 
+  return excerpts
+end
+
+
+
+puts profile_grabber
+
+
+
+
+
+
+
 
 def tag_grabber
   profiles = []
@@ -40,21 +63,10 @@ def tag_grabber
   return tag_line
 end
 
-def excerpt_grabber
-  profiles = []
-  doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
-  profiles << doc.search(".excerpt p").collect{|e| e.text.strip }
-  excerpt = []
-  profiles.each do |excerpt_name|
-    excerpt << {:excerpt => excerpt_name} 
-  end
-  return excerpt
-end
-
 def image_url_grabber
   profiles = []
   doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
-  profiles << doc.search("src").collect{|e| e.text.strip }
+  profiles << doc.search(" ").collect{|e| e.text.strip }
   image_url = []
   profiles.each do |image_url_name|
     image_url << {:image_url => image_url_name} 
@@ -65,7 +77,7 @@ end
 def profile_url_grabber
   profiles = []
   doc = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/").read)
-  profiles << doc.search(".blog-thumb a").collect{|e| e.text.strip }
+  profiles << doc.search("#random-full-width-content").collect{|e| e.text.strip }
   profile_url = []
   profiles.each do |profile_url_name|
     profile_url << {:profile_url => profile_url_name} 
@@ -74,7 +86,7 @@ def profile_url_grabber
 end
 
 # puts excerpt_grabber
-puts image_url_grabber
+# puts image_url_grabber
 # puts profile_url_grabber
 
 
